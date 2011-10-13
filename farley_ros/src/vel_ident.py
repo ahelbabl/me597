a2d9bda83a3fd609995b72d087b4d69e946640cf
Iain Peet <iain@ipeet.org>
2011-10-13 19:38:13 -0400

    vel_ident now prints data to ~/response.dat, for matlabbing

--- farley_ros/src/vel_ident.py
+++ farley_ros/src/vel_ident.py
@@ -9,22 +9,28 @@ import rospy
 from geometry_msgs.msg import Twist
 
 from speedometer import Speedometer
+import os
 
 rospy.init_node('vel_ident')
 
 cmdVel = rospy.Publisher('/clearpath/robots/default/cmd_vel', Twist)
 startTime = rospy.get_rostime()
 rate = rospy.Rate(20)
+outfile = open('{0}/response.dat'.format(os.environ['HOME']), 'w')
+curCmd = 0.0
 
 def speedCb(spd, time):
-  print("t: {0}, v: {1}".format((time - startTime).to_sec(), spd))
+  outfile.write("{0} {1} {2}\n".format( (time-startTime).to_sec(), curCmd, spd))
 
 spd = Speedometer(speedCb)
 
 while not rospy.is_shutdown():
   cmd = Twist()
+  curCmd = 0.0
   if int( 0.25 * (rospy.get_rostime() - startTime).to_sec() ) % 2 == 1:
-    cmd.linear.x = 50.0
+    curCmd = 50.0
+
+  cmd.linear.x = curCmd
   cmdVel.publish(cmd) 
   rate.sleep()
 

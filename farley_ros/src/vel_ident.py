c6f5809da66b87eadc5fb541fac0bc862bc090cb
Iain Peet <iain@ipeet.org>
2011-10-13 18:51:28 -0400

    vel_ident now produces a square wave.

--- farley_ros/src/vel_ident.py
+++ farley_ros/src/vel_ident.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python
 #
-# Implements a simple proportional velocity controller, for use in drive
-# system identifcation, for more sophisticated velocity control design.
+# Subjects the robot to a square wave, so that we can derive motor model constants
+# empirically.
 
 import roslib; roslib.load_manifest('clearpath_horizon')
 import rospy
 
 from geometry_msgs.msg import Twist
 from clearpath_horizon.msg import RawEncoders
-from velocity_control import VelocityControl
 
 rospy.init_node('vel_ident')
-
-c = VelocityControl()
 
 def encodersCb(encoders):
   if len(encoders.ticks) != 1:
     raise Exception("Robot should only have 1 encoder, but found {0}".
         format(len(encoders.ticks)))
-  c.measurement(encoders.ticks[0])
 
 rospy.Subscriber('/clearpath/robots/default/data/raw_encoders', 
     RawEncoders, encodersCb)
 
-c.execute()
+cmdVel = rospy.Publisher('/clearpath/robots/default/cmd_vel', Twist)
+startTime = rospy.get_rostime()
+rate = rospy.Rate(20)
+
+while not rospy.is_shutdown():
+  cmd = Twist()
+  if int( 0.25 * (rospy.get_rostime() - startTime).secs ) % 2 == 1:
+    cmd.linear.x = 50.0
+  cmdVel.publish(cmd) 
+  rate.sleep()
 

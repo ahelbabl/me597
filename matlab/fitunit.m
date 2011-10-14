041aa9d31f94ea053bd09b9860e4ede88ae1e936
Iain Peet <iain@ipeet.org>
2011-10-14 16:56:41 -0400

    Fixed math error, correcting Z transfer function

--- matlab/fitunit.m
+++ matlab/fitunit.m
@@ -103,5 +103,5 @@ am = coeffs(2)
 
 % Create continuous and discrete time system models:
 sysc = tf([Km*am], [1 am]);
-sysd = tf([(Km*am*ts)/(am*ts + 2)], [1 (am*ts-2)/(am*ts+2)], ts);
+sysd = tf((Km*am*ts)/(am*ts + 2)*[1 1], [1 (am*ts-2)/(am*ts+2)], ts);
 

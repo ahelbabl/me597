03c06b60eea08aea6e35a17e96af633c4b146395
Iain Peet <iain@ipeet.org>
2011-10-14 20:39:00 -0400

    Completed good PI control.
    - Fixed my mistake in the integrator which was causing instability
    - Added simple low-pass filter to velocity measurements, which improved
    performance substantially.

--- farley_ros/src/vel_ctrl_test.py
+++ farley_ros/src/vel_ctrl_test.py
@@ -10,9 +10,17 @@ from geometry_msgs.msg import Twist
 
 from velocity_control import VelocityControl
 import os
+import matplotlib.pyplot as p
 
 rospy.init_node('vel_ident')
 
 c = VelocityControl()
 c.setVelocity(0.2);
+
+rospy.sleep(2.0)
+c.start();
+rospy.sleep(120.0);
+c.stop();
+p.plot(c.velRecord)
+p.show()
 rospy.spin()

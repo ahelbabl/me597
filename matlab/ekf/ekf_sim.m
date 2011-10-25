c4b40b47986b3a68adcd65ecea77d48946fa657e
Iain Peet <iain@ipeet.org>
2011-10-25 17:54:59 -0400

    Extended kalman filter simulation functional

--- matlab/ekf/ekf_sim.m
+++ matlab/ekf/ekf_sim.m
@@ -7,9 +7,9 @@ ts = 0.05;  % sim timestep [s]
 len = 0.265; % robot length [m]
 
 % Process covariance.  (assume independence):
-Q = diag([0.01 0.01 0.001 0.001]);
+Q = diag([0.01 0.01 0.001 0.001].^2);
 % Measurement covariance:
-R = diag([0.05 0.1 0.1 0.1]);
+R = diag([0.05 0.1 0.1 0.1].^2);
 
 % Sample count.  Because the discrete velocity model needs to look back
 % 5 samples, we add 5 samples and set t=0 at sample 5
@@ -24,12 +24,17 @@ xi = zeros(4,n);  % ideal state
 xi(:,to) = [0 0 0 0];
 x = xi;  % true state, with process noise
 
-% Measurements.  Direct, in same order as state vector.
-m = zeros(4,n);
-
 % Input signal.  Inputs are [motor %, steering angle (rad)]
 u = [ones(1,n)*100; ones(1,n)*0.3];
 u(:,1:to) = zeros(2,to);
+
+% Measurements.  Direct, in same order as state vector.
+m = zeros(4,n);
+
+% Kalman estimates:
+xe = xi;  % Estimate means, start with correct i.c.
+Pe = zeros(4,4,n);
+Pe(:,:,to) = Q;  % Initial condition covariance
 
 % Velocity model:
 load('vel_coeffs.mat');
@@ -53,15 +58,43 @@ for k = to+1:n
     x(2,k) = x(2,k-1) + x(1,k-1)*sin(u(2,k-1))*ts/len;
     x(3,k) = x(3,k-1) + x(1,k-1)*cos(x(2,k-1))*ts;
     x(4,k) = x(4,k-1) + x(1,k-1)*sin(x(2,k-1))*ts;
-    x(:,k) = x(:,k) + Q*randn(4,1);
+    x(:,k) = x(:,k) + sqrt(Q)*randn(4,1); 
 
     % Measurements:
-    m(:,k) = x(:,k) + R*randn(4,1);
+    m(:,k) = x(:,k) + sqrt(R)*randn(4,1);
 
+    % A priori state estimate:
+    xp = zeros(4,1);
+    xp(1) = zn(1)*u(1,k-5) + zn(2)*u(1,k-4) - zd(2)*xe(1,k-1);
+    xp(2) = xe(2,k-1) + xe(1,k-1)*sin(u(2,k-1))*ts/len;
+    xp(3) = xe(3,k-1) + xe(1,k-1)*cos(xe(2,k-1))*ts;
+    xp(4) = xe(4,k-1) + xe(1,k-1)*sin(xe(2,k-1))*ts;
 
+    % Jacobian of current state w.r.t previous state:
+    A = [-zd(2)            0                            0 0;
+         ts*sin(u(2,k-1))  1                            0 0;
+         ts*cos(xe(2,k-1)) -xe(1,k-1)*ts*sin(xe(2,k-1)) 1 0;
+         ts*sin(xe(2,k-1)) xe(1,k-1)*ts*cos(xe(2,k-1))  0 1; ];
+
+    % Jacobian of state w.r.t process noise:
+    W = eye(4);
+    % Jacobian of measurements w.r.t state:
+    H = eye(4);
+    % Jacobian of measurements w.r.t measurement noise:
+    V = eye(4);
+
+    % A priori covariance:
+    Pp = A*Pe(:,:,k-1)*A' + W*Q*W';
+
+    % Kalman gain:
+    K = Pp*H'*inv(H*Pp*H' + V*R*V');
+
+    % EKF state estimate:
+    xe(:,k) = xp + K*(m(:,k) - xp);
+    Pe(:,:,k) = (eye(4) - K*H)*Pp;
 end
 
-% Plot real state vs. ideal state vs. measurements:
+% Plot true state vs. ideal state vs. measurements:
 figure(1);
 clf();
 
@@ -89,5 +122,38 @@ plot(xi(3,:),xi(4,:));
 plot(x(3,:),x(4,:),'r');
 axis square
 axis equal
+hold off
+
+% Plot EKF estimate 1 sigma s.d bounds vs. true state
+figure(2);
+clf();
+
+%Velocity
+subplot(3,1,1);
+hold on
+plot(t,m(1,:),'g');
+plot(t,xe(1,:)+sqrt(reshape(Pe(1,1,:),size(t))));
+plot(t,xe(1,:)-sqrt(reshape(Pe(1,1,:),size(t))));
+plot(t,x(1,:),'r');
+hold off
+
+subplot(3,1,2);
+hold on
+plot(t,m(2,:),'g');
+plot(t,xe(2,:)+sqrt(reshape(Pe(2,2,:),size(t))));
+plot(t,xe(2,:)-sqrt(reshape(Pe(2,2,:),size(t))));
+plot(t,x(2,:),'r');
+hold off
+
+subplot(3,1,3);
+hold on
+plot(t,m(3,:),'g');
+plot(t,m(4,:),'k');
+plot(t,xe(3,:)+sqrt(reshape(Pe(3,3,:),size(t))));
+plot(t,xe(3,:)-sqrt(reshape(Pe(3,3,:),size(t))));
+plot(t,xe(4,:)+sqrt(reshape(Pe(4,4,:),size(t))),'c');
+plot(t,xe(4,:)-sqrt(reshape(Pe(4,4,:),size(t))),'c');
+plot(t,x(3,:),'r');
+plot(t,x(4,:),'y');
 hold off
 

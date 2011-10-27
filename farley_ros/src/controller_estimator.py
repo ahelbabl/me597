5ea816fa6bd10c62e63be9604cad420ee878c518
Iain Peet <iain@ipeet.org>
2011-10-27 19:53:44 -0400

    Preliminary implementation of EKF in python

--- farley_ros/src/controller_estimator.py
+++ farley_ros/src/controller_estimator.py
@@ -24,6 +24,9 @@ class ControllerEstimator:
 
     self.running = False
     self.ts = 0.05 # Sample time [s]
+    self.length = 0.265 # Robot length [m]
+
+    # Velocity control state:
 
     # Proportional control constants
     self.kp = 143.9
@@ -34,6 +37,8 @@ class ControllerEstimator:
     # Reference and control signals:
     self.velRef = None  # [m/s]
     self.velCtrl = None # [-100,100]
+
+    # Steering control state:
 
     # Proportional control constant for steering angle.
     self.kSteer = 0.5
@@ -50,6 +55,34 @@ class ControllerEstimator:
     self.velRecord = np.array([0])
     self.xRecord = np.array([0])
     self.yRecord = np.array([0])
+
+    # EKF State:
+    # Latest measurements.  Set to None when consumed for EKF estimate. 
+    self.lastPose = None
+    self.lastVel = None
+
+    # Process covariance:
+    self.Q = np.array([[0.0005, 0, 0, 0],  
+                       [0, 0.15, 0, 0],
+                       [0, 0, 0.07, 0],
+                       [0, 0, 0, 0.07]])
+    # Measurement covariance:
+    self.R = np.array([[0.0035, 0, 0, 0],
+                       [0, 0.7, 0, 0],
+                       [0, 0, 0.7, 0],
+                       [0, 0, 0, 0.7]])
+
+    # Zero initial state
+    self.stateEst = np.zeros((4,1))
+    self.velOutDelay = [0 0 0 0]
+    # ... with low confidence
+    self.P = np.ones((4,4))
+
+    # Velocity model params:
+    Km = 0.003267973309
+    am = 6.779750386209
+    self.velNum = (Km*am*self.ts)/(am*self.ts + 2)
+    self.velDen = (am*self.ts-2)/(am*self.ts+2)
 
   def setVelocity(self, ref):
     """ Set the velocity reference """
@@ -90,6 +123,7 @@ class ControllerEstimator:
       velCmd.linear.x = self.velCtrl
     if not self.steerCtrl is None:
       velCmd.angular.z = self.steerCtrl
+    self.velOutDelay = self.velOutDelay[2:] + [self.velCtrl]
     self.cmdPub.publish(velCmd)
 
   def _saturate(self, val, limit):
@@ -99,6 +133,62 @@ class ControllerEstimator:
     if val < -limit:
        return -limit
     return val
+
+  def _wrapAngle(self, ang):
+    """ Wrap an angle (radian) to [-pi, pi] """
+    while ang > math.pi:
+        ang -= math.pi
+    while ang < -math.pi:
+        ang += math.pi
+    return ang
+
+  def _halt(self):
+    self.setVelocity(0)
+    self.setSteeringAngle(0)
+
+  def _ekfUpdate(self):
+    if (self.lastVel is None) or (self.lastPose is None):
+      # Not enough info to iterate kalman
+      return
+
+    # Measurements:
+    m = np.array([[self.lastVel], 
+            [self.lastPose.Yaw], 
+            [self.lastPose.X], 
+            [self.lastPose.Y]])
+
+    # A priori state estimate:
+    xp = np.zeros((4,1))
+    xp[0] = self.velNum*(self.velOutDelay[-4] + self.velOutDelay[-3]) -
+        self.velDen*self.stateEst[0]
+    xp[1] = self.stateEst[1] + 
+        self.stateEst[0]*math.sin(self.steerCtrl)*self.ts / self.length
+    xp[2] = self.stateEst[2] +
+        self.stateEst[0]*math.cos(self.stateEst[1])*self.ts
+    xp[3] = self.stateEst[3] +
+        self.stateEst[0]*math.sin(self.stateEst[1])*self.ts
+
+    # Jacobian of current state w.r.t previous state:
+    A = np.zeros((4,4))
+    A[0,0] = -self.velDen
+    A[1,0] = self.ts * math.sin(self.steerctrl) / self.length
+    A[1,1] = 1
+    A[2,0] = self.ts * math.cos(self.stateEst[1])
+    A[2,1] = -self.ts * self.stateEst[0] * math.sin(self.steerCtrl)
+    A[2,2] = 1
+    A[3,0] = self.ts * math.sin(self.steerCtrl)
+    A[3,1] = self.ts * self.stateEst[0] * math.cos(self.steerCtrl)
+    A[3,3] = 1
+
+    # A priori covariance:
+    Pp = A * self.P * A.transpose() + self.Q
+
+    # Kalman Gain:
+    K = Pp * (Pp + self.R).inv()
+
+    # State estimates:
+    self.stateEst = xp + K*(m - xp)
+    self.P = (np.identity(4) - K)*Pp
 
   def _velocityCb(self, vel, time):
     """ Accepts new measurement data, and adjusts the control signal """
@@ -122,20 +212,10 @@ class ControllerEstimator:
     self._publish()
     return
 
-  def _wrapAngle(self, ang):
-    """ Wrap an angle (radian) to [-pi, pi] """
-    while ang > math.pi:
-        ang -= math.pi
-    while ang < -math.pi:
-        ang += math.pi
-    return ang
-
-  def _halt(self):
-    self.setVelocity(0)
-    self.setSteeringAngle(0)
-
   def _poseCb(self, pose):
     """ Update steering command using current pose """
+    self.lastPose = pose
+
     if not self.running:
       return
     if (self.xRef is None) or (self.yRef is None) or (self.hRef is None):
@@ -143,6 +223,8 @@ class ControllerEstimator:
 
     self.xRecord = np.concatenate((self.xRecord, np.array([pose.X])))
     self.yRecord = np.concatenate((self.yRecord, np.array([pose.Y])))
+    self.outfile.write('{0} {1} {2} {3}\n'.format(
+          pose.header.stamp.to_sec(), pose.X, pose.Y, pose.Yaw))
    
     # Calculate heading error
     ang = (pose.Yaw * math.pi / 180)
